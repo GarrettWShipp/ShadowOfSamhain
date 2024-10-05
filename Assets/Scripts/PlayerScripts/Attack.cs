bd97cede7f221be804d04f569837ea19ef575031
Garrett Shipp <garwad9@gmail.com>
2024-10-05 17:30:27 -0500

    player prefab

--- Assets/Scripts/PlayerScripts/Attack.cs
+++ Assets/Scripts/PlayerScripts/Attack.cs
@@ -2,6 +2,7 @@ using SuperPupSystems.Helper;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public abstract class Attack : MonoBehaviour
 {
@@ -16,6 +17,7 @@ public abstract class Attack : MonoBehaviour
     public Vector2 squareAttackRange;
     public Animator attackAnim;
     [HideInInspector] public float attackNext;
+    private float m_angle;
 
     // Start is called before the first frame update
     void Start()
@@ -26,8 +28,40 @@ public abstract class Attack : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
-        
+        if (m_playerManager.movement == new Vector2(1f, 0f))
+        {
+            m_playerManager.littleguy.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+        if (m_playerManager.movement == new Vector2(0f, 1f))
+        {
+            m_playerManager.littleguy.transform.localRotation = Quaternion.Euler(0, 0, 90);
+        }
+        if (m_playerManager.movement == new Vector2(-1f, 0f))
+        {
+            m_playerManager.littleguy.transform.localRotation = Quaternion.Euler(0, 0, 180);
+        }
+        if (m_playerManager.movement == new Vector2(0f, -1f))
+        {;
+            m_playerManager.littleguy.transform.localRotation = Quaternion.Euler(0, 0, 270);
+        }
+
+        if (m_playerManager.movement == new Vector2(1f, 1f))
+        {
+            m_playerManager.littleguy.transform.localRotation = Quaternion.Euler(0, 0, 45);
+        }
+        if (m_playerManager.movement == new Vector2(-1f, -1f))
+        {
+            m_playerManager.littleguy.transform.localRotation = Quaternion.Euler(0, 0, 225);
+        }
+        if (m_playerManager.movement == new Vector2(-1f, 1f))
+        {
+            m_playerManager.littleguy.transform.localRotation = Quaternion.Euler(0, 0, 135);
+        }
+        if (m_playerManager.movement == new Vector2(1f, -1f))
+        {
+            m_playerManager.littleguy.transform.localRotation = Quaternion.Euler(0, 0, 315);
+        }
+
     }
     public void OnAttackCircle()
     {
