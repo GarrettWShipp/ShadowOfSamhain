971849aeb9b3a3c953ca4cf7073681785121918b
DylanGHilliard <ghilliard69420@gmail.com>
2024-10-05 23:02:38 -0500

    audio

---
+++ Assets/Scripts/AudioMusic.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class AudioMusic : MonoBehaviour
+{
+
+   
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
